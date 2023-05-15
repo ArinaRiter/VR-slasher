d553954a42279dc459fc26596ce7cc53cd264d60
annagrrrr <117434839+annagrrrr@users.noreply.github.com>
2023-05-15 22:41:58 +0300

    Added Dying Lol

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _health;
+    [SerializeField] private float _maxHealth = 16; //
 
+    private void Start()
+    {
+        _health = _maxHealth;
+    }
     public void TakeDamage(float damage)
     {
         if (_health - damage <= 0)
         {
             Debug.Log("Died");
+            SceneManager.LoadScene("Dead"); //
             return;
         }
         else
