5d93734c9f7414e9ad3df18902acb2d0b9ecb586
annagrrrr <117434839+annagrrrr@users.noreply.github.com>
2023-05-23 17:47:49 +0300

    Menu and maybe loading screen but idk

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,13 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
-    public void StartBtn()
+    public GameObject LoadingScreen;
+    public Image LoadingBarFill;
+
+    //public void LoadScene(int sceneId)
+    //{
+    //    StartCoroutine(LoadSceneAsync(sceneId));
+    //}
+
+    IEnumerator LoadSceneAsync(int sceneId)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneId);
+        LoadingScreen.SetActive(true);
+        while (!operation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(operation.progress / 0.9f);
+            LoadingBarFill.fillAmount = progressValue;
+            yield return null;
+        }
+    }
+    public void StartBtn(int sceneId)
     {
         SceneManager.LoadScene("Grey Box");
+        StartCoroutine(LoadSceneAsync(sceneId));//
     }
 
     public void ExitBtn()
