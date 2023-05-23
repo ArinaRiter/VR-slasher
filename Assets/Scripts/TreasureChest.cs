199cf8f49a686d1a998defef345945843913a289
annagrrrr <117434839+annagrrrr@users.noreply.github.com>
2023-05-23 22:02:12 +0300

    ae

--- Assets/Scripts/TreasureChest.cs
+++ Assets/Scripts/TreasureChest.cs
@@ -1,24 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class TreasureChest : MonoBehaviour
-{
-    private GameObject _player = GameObject.FindWithTag("player");
-    private static readonly int near = Animator.StringToHash("playerIsNear");
-    [SerializeField] private Animator animator;
-    [SerializeField] private float _range;
-    private GameObject _light = GameObject.FindWithTag("TreasureLight");
-    void Update()
-    {
-        if (Vector3.Distance(gameObject.transform.position, _player.transform.position) < _range)
-        {
-            animator.SetBool(near, true);
-            _light.SetActive(true);
-        }
-        //else
-        //{
-        //    animator.SetBool(near, false);
-        //}
-    }
-}
+//public class TreasureChest : MonoBehaviour
+//{
+//    private GameObject _player = GameObject.FindWithTag("player");
+//    private static readonly int near = Animator.StringToHash("playerIsNear");
+//    [SerializeField] private Animator animator;
+//    [SerializeField] private float _range;
+//    private GameObject _light = GameObject.FindWithTag("TreasureLight");
+//    void Update()
+//    {
+//        if (Vector3.Distance(gameObject.transform.position, _player.transform.position) < _range)
+//        {
+//            animator.SetBool(near, true);
+//            _light.SetActive(true);
+//        }
+//        //else
+//        //{
+//        //    animator.SetBool(near, false);
+//        //}
+//    }
+//}
